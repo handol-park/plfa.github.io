d15a627dd4d35487babeb2daa168c764fccf1372
Handol Park <handolpark12@gmail.com>
2023-05-20 17:24:17 -0700

    Install the prerequisites in the devShell

--- flake.nix
+++ flake.nix
@@ -7,16 +7,26 @@
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
+      debug = true;
+
       imports = [
         ./hello/flake-module.nix
       ];
       systems = [ "x86_64-linux" "aarch64-darwin" ];
-      perSystem = { config, self', inputs', ... }: {
+      perSystem = { config, self', inputs', pkgs, ... }: {
         # Per-system attributes can be defined here. The self' and inputs'
         # module parameters provide easy access to attributes of the same
         # system.
 
         packages.figlet = inputs'.nixpkgs.legacyPackages.figlet;
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            ghc
+            cabal-install
+            agda
+          ];
+        };
       };
       flake = {
         # The usual flake attributes can be defined here, including system-
